ad7b540f5df02ce4df1ff3e5c97cedf592703a52
Valeh <kimyagersual@gmail.com>
2022-11-30 21:59:34 +0400

    Ilk Consol  projelerimi yazdim

---
+++ Noyabr/30.11/IlkCode/IlkCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam Xos gelmisiniz");
+            Console.Write("Daxil edin :");
+            Console.Read();
+        }
+    }
+}
---
+++ Noyabr/30.11/NewTask$Deyisen/NewTask$Deyisen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTask_Deyisen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var red = "Qirmizi";
+            var car = "Avtomobil";
+            var exp = "Bahali";
+            var fon = "Telefon";
+            Console.WriteLine(red +" "+ car);
+            Console.Write(exp+ " "+ fon);
+            Console.Read();
+        }
+    }
+}
