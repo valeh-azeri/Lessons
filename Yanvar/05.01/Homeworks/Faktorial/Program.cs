c6f35d9e73c0d5cd5065b107734b6f0f298a5b08
Valeh <kimyagersual@gmail.com>
2023-01-01 20:44:43 +0400

    New excange in january

---
+++ Yanvar/05.01/Homeworks/Faktorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int fakt=1, girilenSay ;
+            Console.Write("Faktorialını tapmaq istədiyiniz sayi  girin : ");
+            girilenSay = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=girilenSay; i++)
+            {
+                fakt = fakt * i;
+                Console.WriteLine(i + "!=" + fakt);
+
+            }
+
+            Console.WriteLine("_________________________");
+            Console.WriteLine(girilenSay+"!=" + fakt);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Yanvar/05.01/Homeworks/Yolka/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yolka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 20; i > 1; i--)
+            {
+                for (int j = i ; j > 1; j--)
+                {
+                  
+                    Console.Write("*"); Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
